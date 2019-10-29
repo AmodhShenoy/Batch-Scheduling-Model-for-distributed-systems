2cbdf1331821902820aa77e672451134f6dda4a4
Amodh Shenoy <amodhshenoy@gmail.com>
2019-10-29 23:34:16 +0530

    BFO almost done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Block-Scheduling-Model-for-distributed-systems
+# Batch-Scheduling-Model-for-distributed-systems
 Implementation of IEEE paper on scheduling of jobs in an distributed environment
 
 [Here](https://ieeexplore.ieee.org/document/7913119) is the link to the paper.
--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,16 @@
 import random
 
-def makespan(sol,mips,mi,m):
-	time_vec = [0 for _ in range(m)]
+def get_time_vec(sol,etc):
+	time_vec = [0 for _ in range(len(etc))]
 	for i in range(len(sol)):
-		time_vec[sol[i]] += mi[i]/mips[sol[i]]
-	return max(time_vec)
+		time_vec[sol[i]] += etc[sol[i]][i]
+	return time_vec
 
-def utilization(sol,mips,mi,m):
-	time_vec = [0 for _ in range(m)]
-	for i in range(len(sol)):
-		time_vec[sol[i]] += mi[i]/mips[sol[i]]
+def makespan(sol,etc):
+	return max(get_time_vec(sol,etc))
+
+def utilization(sol,etc):
+	time_vec = get_time_vec(sol,etc)
 	mspan = max(time_vec)
 	mpat = sum(time_vec)/m
 	return mpat/mspan
@@ -17,11 +18,45 @@ def utilization(sol,mips,mi,m):
 def GA(pop,n,m):
 	pass
 
-def BFO(pop,n,m):
+def mkspn_sort(elem):
+	return elem[-1]
+
+def BFO(pop,etc):
 	#Setting algorithm constraints
+	m = len(etc)
+	n = len(etc[0])
+	S = len(pop)
 	ned = 3 		#no of elimination steps
 	nc  = 3			#no of chemotaxis steps
 	ped = 0.5		#probability of bacteria elimination
+
+	for _ in range(ned):
+		
+		for i in range(len(pop)):
+			#tumble and move
+			for _ in range(nc):
+				time_vec = get_time_vec(pop[i],etc)
+				max_node = time_vec.index(max(time_vec))
+				min_node = time_vec.index(min(time_vec))
+				tasks = []
+				for i in pop[i]:
+					if i==max_node:
+						tasks.append(i)
+				tumbler = tasks[0]
+				for t in tasks:
+					if etc[max_node][t]<etc[max_node][tumbler]:
+						tumbler = t
+				pop[i][tumbler] = min_node
+			pop[i].append(makespan(pop[i],etc))
+		pop.sort(key=mkspn_sort)
+		for i in range(len(pop)):
+			pop[i].pop()
+
+		#reproduce
+		
+		
+		#eliminate
+		pass
 
 def main():
 	#loading the speed vector
@@ -50,18 +85,18 @@ def main():
 		pop.append(l)
 
 	#running BFO algorithm
-	sol_bfo = BFO(pop,n,m)
+	sol_bfo = BFO(pop,etc)
 	print("Solution from BFO algorithm:")
 	print(sol_bfo)
-	print("Makespan:",makespan(sol,mips,mi,m))
-	print("Utilization:",utilization(sol,mips,mi,m),"\n")
+	print("Makespan:",makespan(sol,etc))
+	print("Utilization:",utilization(sol,etc),"\n")
 
-	#running GA algorithm
-	sol_ga = GA(pop,n,m)
-	print("Solution from GA algorithm:")
-	print(sol_ga)
-	print("Makespan:",makespan(sol,mips,mi,m))
-	print("Utilization:",utilization(sol,mips,mi,m),"\n")
+	# #running GA algorithm
+	# sol_ga = GA(pop,n,m)
+	# print("Solution from GA algorithm:")
+	# print(sol_ga)
+	# print("Makespan:",makespan(sol,etc))
+	# print("Utilization:",utilization(sol,etc),"\n")
 
 
 	for i in pop:
