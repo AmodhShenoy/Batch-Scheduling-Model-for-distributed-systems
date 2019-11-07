9ccf27137c6339d7f619f9425b63c47db1cccbc7
Amodh Shenoy <amodhshenoy@gmail.com>
2019-11-07 20:45:35 +0530

    BFO working

--- scheduler.py
+++ scheduler.py
@@ -2,13 +2,13 @@ import random
 import math 
 
 def get_time_vec(sol,etc):
-	print(sol)
+	# print(sol)
 	time_vec = [0 for _ in range(len(etc))]
 	for i in range(len(sol)):
 		time_vec[sol[i]] += etc[sol[i]][i]
 	return time_vec
 
-def makespan(sol,etc):
+def makespan(sol):
 	return max(get_time_vec(sol,etc))
 
 def utilization(sol,etc):
@@ -24,39 +24,23 @@ def GA(pop,n,m):
 def mkspn_sort(elem):
 	return elem[-1]
 
-def sort_pop(pop,etc):
-	# print("Before sort:")
-	# for i in range(len(pop)):
-	# 	print(pop[i])
-	for i in range(len(pop)):
-		mkspn = makespan(pop[i],etc)
-		pop[i].append(mkspn)
-	pop.sort(key=mkspn_sort)
-	for i in range(len(pop)):
-		pop[i].pop()
-	# print("After sort:")
-	# for i in pop:
-	# 	print(i)
-	return pop
-
-
 def BFO(pop,etc):
 	#Setting algorithm constraints
 	m = len(etc)
 	n = len(etc[0])
 	S = len(pop)
-	ned = 30 		#no of elimination steps
-	nc  = 30			#no of chemotaxis steps
-	ped = 0.5		#probability of bacteria elimination
+	ned = 100	#no of elimination steps
+	nc  = 10		#no of chemotaxis steps
+	ped = 0.2	#probability of bacteria elimination
 
 	for df in range(ned):
-		print(df)
-		#chemotaxis
-		# print("Before chemotaxis")
-		# for i in pop:
-		# 	print(i)
+		# print("Iteration",df+1)
+		# print("Before tumble")
+		# for j in pop:
+		# 	print(j)
+		# print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.')
 		for i in range(len(pop)):
-			#tumble and move
+			#tumble and move			
 			for _ in range(nc):
 				time_vec = get_time_vec(pop[i],etc)
 				max_node = time_vec.index(max(time_vec))
@@ -70,45 +54,35 @@ def BFO(pop,etc):
 					if etc[max_node][t]<etc[max_node][tumbler]:
 						tumbler = t
 				pop[i][tumbler] = min_node
-		pop = sort_pop(pop,etc)
-		print('----------------------------------')
-	
+		
+		pop.sort(key = makespan)
 
 		#reproduce
 		pop = pop[:int(math.ceil(len(pop)/2))] + pop[:int(math.ceil(len(pop)/2))]
-		for i in pop:
-			print(i) 
-
-
-
-
-
-		# step = math.ceil(len(pop)/2)
-		# for i in range(step,len(pop)):
-		# 	pop[i] = pop[i-step]
+		# for i in pop:
+		# 	print(i)
 		# random.shuffle(pop)
+		# print(len(pop))
 
+		#eliminate and replace
+		ct = int(len(pop)*ped)
+		for _ in range(ct):
+			pop.pop(int(random.random()*len(pop)))
 
-
-
-
-
-
-	# 	print("Done reproducing")
-
-	# 	#eliminate and replace
-	# 	for _ in range(int(len(pop)*ped)):
-	# 		pop.pop(int(random.random()*len(pop)))
-	# 	for _ in range(int(len(pop)*ped)):
-	# 		l = []
-	# 		for _ in range(len(pop[0])):
-	# 			l.append(int(random.random()*m))
-	# 	print("Done eliminate")
-	# for i in range(len(pop)):
-	# 	pop[i].append(makespan(pop[i],etc))
-
-	# pop.sort(key=mkspn_sort)
-	return pop[0][:-1]
+		# print("After eliminating")
+		# print(len(pop))
+		for _ in range(ct):
+			l = []
+			for _ in range(len(pop[0])):
+				l.append(int(random.random()*m))
+			# print(l)
+			pop.append(l)
+		# print('------------------------------------------')
+		# for i in pop:
+		# 	print(i)
+		# print('------------------------------------------')
+	pop.sort(key=makespan)
+	return pop[0][:-1],pop
 
 
 
@@ -124,6 +98,7 @@ def main():
 	n = len(mi)
 
 	#generating the ETC Matrix
+	global etc
 	etc = [[0 for _ in range(n)] for _ in range(m)]
 	for i in range(m):
 		for j in range(n):
@@ -136,24 +111,25 @@ def main():
 		l=[]
 		for _ in range(n):
 			l.append(int(random.random()*m))
+		# pop.append([3, 0, 0, 2, 4, 0, 1, 2, 1])
 		pop.append(l)
 
 	#running BFO algorithm
-	sol_bfo = BFO(pop,etc)
+	sol_bfo,pop1 = BFO(pop,etc)
 	print("Solution from BFO algorithm:")
 	print(sol_bfo)
-	print("Makespan:",makespan(sol_bfo,etc))
+	print("Makespan:",makespan(sol_bfo))
 	print("Utilization:",utilization(sol_bfo,etc),"\n")
 
 	# #running GA algorithm
-	# sol_ga = GA(pop,n,m)
-	# print("Solution from GA algorithm:")
-	# print(sol_ga)
-	# print("Makespan:",makespan(sol,etc))
-	# print("Utilization:",utilization(sol,etc),"\n")
+	sol_ga = GA(pop,n,m)
+	print("Solution from GA algorithm:")
+	print(sol_ga)
+	print("Makespan:",makespan(sol_ga))
+	print("Utilization:",utilization(sol_ga,etc),"\n")
 
 
-	for i in pop:
+	for i in pop1:
 		print(i)
 
 if __name__=='__main__':
