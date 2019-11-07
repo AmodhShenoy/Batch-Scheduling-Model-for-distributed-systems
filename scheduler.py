618df1bded015d02d5363fa06c49ca260ba7a758
Amodh Shenoy <amodhshenoy@gmail.com>
2019-11-08 03:07:14 +0530

    Code all sett :)

--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,6 @@ import random
 import math 
 
 def get_time_vec(sol,etc):
-	# print(sol)
 	time_vec = [0 for _ in range(len(etc))]
 	for i in range(len(sol)):
 		time_vec[sol[i]] += etc[sol[i]][i]
@@ -18,8 +17,64 @@ def utilization(sol,etc):
 	mpat = sum(time_vec)/m
 	return mpat/mspan
 
-def GA(pop,n,m):
-	pass
+def reset(pop):
+	for i in pop:
+			i.pop(-1)
+	return pop
+
+#for GA
+def calcAndSort(pop,etc):
+	for i in pop:
+			i.append(makespan(i))
+		
+	pop.sort(key=lambda x:x[-1])
+	return pop
+
+def p(pop):
+	for i in pop:
+		print(i)
+
+def GA(pop,etc):
+	m = len(etc)
+	n = len(etc[0])
+	genes="01234"
+	newgen=[]
+	count=1
+
+	while count!=len(newgen):
+		newgen=[]
+		
+		#generation of makespan for each solution and then sorting(asc)
+		pop=calcAndSort(pop,etc)
+
+		#selection
+		selected=math.ceil(len(pop)/2)
+		pop=pop[:selected]
+
+		pop=reset(pop)
+
+		#crossover
+		for _ in range(m*2):
+			parent1=random.choice(pop)
+			parent2=random.choice(pop)
+			cp1=random.randint(0,n-1)
+			cp2=random.randint(0,n-1)
+			maxCp=max(cp1,cp2)
+			minCp=min(cp1,cp2)
+			child1=parent1[:minCp]+parent2[minCp:maxCp]+parent1[maxCp:]
+			#mutation
+			mpoint=random.randint(0,n-1)
+			child1[mpoint]=int(random.choice(genes))
+			child2=parent2[:minCp]+parent1[minCp:maxCp]+parent2[maxCp:]
+			newgen.extend([child1,child2])
+
+		select=len(newgen)-(count-1)
+		newgen=newgen[:select]
+		pop.extend(newgen)
+		count+=1
+	
+	
+	return pop[0]
 
 def mkspn_sort(elem):
 	return elem[-1]
@@ -29,16 +84,11 @@ def BFO(pop,etc):
 	m = len(etc)
 	n = len(etc[0])
 	S = len(pop)
-	ned = 100	#no of elimination steps
-	nc  = 10		#no of chemotaxis steps
+	ned = 500	#no of elimination steps
+	nc  = 10	#no of chemotaxis steps
 	ped = 0.2	#probability of bacteria elimination
 
 	for df in range(ned):
-		# print("Iteration",df+1)
-		# print("Before tumble")
-		# for j in pop:
-		# 	print(j)
-		# print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>.')
 		for i in range(len(pop)):
 			#tumble and move			
 			for _ in range(nc):
@@ -46,54 +96,44 @@ def BFO(pop,etc):
 				max_node = time_vec.index(max(time_vec))
 				min_node = time_vec.index(min(time_vec))
 				tasks = []
-				for i in pop[i]:
-					if i==max_node:
-						tasks.append(i)
+				for k in range(n):
+					if pop[i][k] == max_node:
+						tasks.append(k)
+
 				tumbler = tasks[0]
 				for t in tasks:
 					if etc[max_node][t]<etc[max_node][tumbler]:
 						tumbler = t
 				pop[i][tumbler] = min_node
-		
 		pop.sort(key = makespan)
 
 		#reproduce
 		pop = pop[:int(math.ceil(len(pop)/2))] + pop[:int(math.ceil(len(pop)/2))]
-		# for i in pop:
-		# 	print(i)
-		# random.shuffle(pop)
-		# print(len(pop))
+		random.shuffle(pop)
 
 		#eliminate and replace
 		ct = int(len(pop)*ped)
 		for _ in range(ct):
 			pop.pop(int(random.random()*len(pop)))
 
-		# print("After eliminating")
-		# print(len(pop))
 		for _ in range(ct):
 			l = []
 			for _ in range(len(pop[0])):
 				l.append(int(random.random()*m))
-			# print(l)
 			pop.append(l)
-		# print('------------------------------------------')
-		# for i in pop:
-		# 	print(i)
-		# print('------------------------------------------')
 	pop.sort(key=makespan)
-	return pop[0][:-1],pop
+	return pop[0][:-1]
 
 
 
 def main():
 	#loading the speed vector
-	node_file = open('nodes')
+	node_file = open('input/nodes')
 	mips = list(map(float,node_file.read().strip().split(',')))
 	m = len(mips)
 
 	#loading the task sizes
-	task_file = open('tasks')
+	task_file = open('input/tasks')
 	mi = list(map(float,task_file.read().strip().split(',')))
 	n = len(mi)
 
@@ -111,26 +151,23 @@ def main():
 		l=[]
 		for _ in range(n):
 			l.append(int(random.random()*m))
-		# pop.append([3, 0, 0, 2, 4, 0, 1, 2, 1])
 		pop.append(l)
 
+	pop.sort(key=makespan)
+	print("Initial makespan:",makespan(pop[0]),"\n")
 	#running BFO algorithm
-	sol_bfo,pop1 = BFO(pop,etc)
+	sol_bfo = BFO(pop,etc)
 	print("Solution from BFO algorithm:")
 	print(sol_bfo)
 	print("Makespan:",makespan(sol_bfo))
 	print("Utilization:",utilization(sol_bfo,etc),"\n")
 
 	# #running GA algorithm
-	sol_ga = GA(pop,n,m)
+	sol_ga = GA(pop,etc)
 	print("Solution from GA algorithm:")
 	print(sol_ga)
 	print("Makespan:",makespan(sol_ga))
 	print("Utilization:",utilization(sol_ga,etc),"\n")
-
-
-	for i in pop1:
-		print(i)
 
 if __name__=='__main__':
 	main()
